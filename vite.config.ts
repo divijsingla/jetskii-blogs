f2745191ff8d193f774637bd6cfd1c50b319ff00
Divij <105979376+divijsingla@users.noreply.github.com>
2025-07-28 15:55:35 +0530

    change repo name

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: "/nerdy-warm-static/",
+  base: "/jetskii-blogs/",
   server: {
     host: "::",
     port: 8080,
